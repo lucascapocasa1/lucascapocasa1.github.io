3cad31e9528cb732ab77ed139113676250d3e80f
lucascapocasa1 <86982967+lucascapocasa1@users.noreply.github.com>
2025-04-10 21:39:18 -0300

    Add files via upload

---
+++ comportamientos.js
@@ -0,0 +1,13 @@
+function redireccionarALinkedin(){
+    /*window.location.href = "https://www.linkedin.com/home"*/
+    window.location.href = "opcion1.html"
+}
+
+function redireccionarAFarmacity(){
+    window.location.href = "https://www.farmacity.com/"
+
+}
+
+function redireccionarALogin(){
+    window.location.href = "login.html"
+}
